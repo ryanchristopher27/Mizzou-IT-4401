e0403d75fdc14c1d3028644c18085b679dfa3387
ryanchristopher27 <ryandchristopher27@gmail.com>
2021-01-19 16:20:31 -0600

    Module 1 Update
    Completed module 1, about me

---
+++ Module1/AboutMe/aboutme.py
@@ -0,0 +1,8 @@
+#About Me
+#Ryan Christopher
+
+print('Ryan Christopher')
+print('I am currently a sophomore studying computer science.')
+print('I am from Springfield, Missouri.')
+print('My favorite sport is soccer and I have played my whole life.')
+print('I am a member of Phi Delta Theta here at Mizzou.')
